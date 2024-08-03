5738e565ab5975057a8cc2f6f50cacccd5ae3838
MarinaBarrov <maari-b@live.com.ar>
2024-08-03 12:23:21 -0300

    configuracion de proyecto

---
+++ src/main/java/PIBackend/Eliseo/EliseoApplication.java
@@ -0,0 +1,13 @@
+package PIBackend.Eliseo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EliseoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EliseoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PIBackend/Eliseo/EliseoApplicationTests.java
@@ -0,0 +1,13 @@
+package PIBackend.Eliseo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliseoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
